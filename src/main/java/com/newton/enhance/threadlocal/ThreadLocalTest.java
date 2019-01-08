1464937c2882235a2fec2c0f57dd6fc865ef59f8
huangjie <curtishuang@163.com>
2019-01-08 17:19:46 +0800

    增加ThreadLocal

--- src/main/java/com/newton/enhance/threadlocal/ThreadLocalTest.java
+++ src/main/java/com/newton/enhance/threadlocal/ThreadLocalTest.java
@@ -6,6 +6,39 @@ package com.newton.enhance.threadlocal;
  * 2）为何threadLocals的类型ThreadLocalMap的键值为ThreadLocal对象，
  * 因为每个线程中可有多个threadLocal变量，就像上面代码中的longLocal和stringLocal；
  * 3）在进行get之前，必须先set，否则会报空指针异常；
+ *
+ *
+ * 最常见的ThreadLocal使用场景为 用来解决 数据库连接、Session管理等。
+ *
+ * （1）private static ThreadLocal<Connection> connectionHolder
+ * = new ThreadLocal<Connection>() {
+ * public Connection initialValue() {
+ *     return DriverManager.getConnection(DB_URL);
+ * }
+ * };
+ *
+ * public static Connection getConnection() {
+ * return connectionHolder.get();
+ * }
+ *
+ *
+ * （2）
+ * private static final ThreadLocal threadSession = new ThreadLocal();
+ *
+ * public static Session getSession() throws InfrastructureException {
+ *     Session s = (Session) threadSession.get();
+ *     try {
+ *         if (s == null) {
+ *             s = getSessionFactory().openSession();
+ *             threadSession.set(s);
+ *         }
+ *     } catch (HibernateException ex) {
+ *         throw new InfrastructureException(ex);
+ *     }
+ *     return s;
+ * }
+ *
+ *
  */
 public class ThreadLocalTest {
 
